9a5c8bb38a65ac5ad0a35f8d36678778959cd363
NothingElsee <marioiuliano2001@gmail.com>
2025-03-31 21:28:33 +0200

    javadocs

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.nothingelsee.Aesthetics"},{"l":"me.nothingelsee.Controller"},{"l":"me.nothingelsee.Database"},{"l":"me.nothingelsee.ENUM"},{"l":"me.nothingelsee.GUI"},{"l":"me.nothingelsee.ImplementazioniPostgresDAO"},{"l":"me.nothingelsee.InterfacceDAO"},{"l":"me.nothingelsee.Model"}];updateSearchResults();
